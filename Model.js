0a8d9e0aa3e9b8e8934c9047a9c0a50d04b9cafb
white321123 <mikhalkov92.92@mail.ru>
2023-12-15 16:25:59 +0300

    Шутки

--- Model.js
+++ Model.js
@@ -1,65 +1,173 @@
-// const inquirer = require('inquirer')
-
-// inquirer.prompt([
-//   { type: 'input', name: 'username', message: 'Знакомство: ' },
-//   {
-//     type: 'list',
-//     name: 'Какой был вопрос',
-//     massage: 'Тут у нас сам вопрос',
-//     choices: [
-//       { name: 'вармант1', value: 2, short: 'молодец' },
-//       { name: 'вариант2', value: 1, short: 'могло быть и лучше' },
-//       { name: 'вариант3', value: 0, short: 'совсем не то' },
-
-//     ],
-//   }
-// ],
-// ).then((answers) => console.log(answers));
-
-const inquirer = require('inquirer') ;
-const { EOL } = require('os')
+const inquirer = require("inquirer");
+const { EOL } = require("os");
 
 async function getQuestions() {
-  return inquirer.prompt([
-    {type: 'input', name: 'username', message: 'Введи имя:'},
-    {
-    type: 'list',
-    name: 'bonuses1',
-    message: 'Что говорит JavaScript, когда его просят рассказать анекдот?',
-    choices: [
-        { name: 'Мой лучший анекдот - это когда async и await зашли в бар...', value: 5, short: 'А ты шаришь!' },
-        { name: 'У меня есть такой хороший анекдот про null, но он undefined', value: 5, short: 'Хорошее чувство юмора ;)' },
-        { name: 'Ничего', value: 0, short: 'Не смешно' },
-        ],   
-    },
-    {
-    type: 'list',
-    name: 'bonuses2',
-    message: 'Ты точно понял все темы?',
-    choices: [
-        { name: 'Всё понял', value: 5, short: 'первый' },
-        { name: 'Половину понял', value: 3, short: 'первый' },
-        { name: 'Ничего не понял', value: 0, short: 'первый' },
-        ],   
-    },
-    {
-      type: 'list',
-      name: 'bonuses3',
-      message: 'Ты точно понял все темы?',
-      choices: [
-          { name: 'Всё понял', value: 5, short: 'первый' },
-          { name: 'Половину понял', value: 3, short: 'первый' },
-          { name: 'Ничего не понял', value: 0, short: 'первый' },
-          ],   
-      },
-])
+	return inquirer.prompt([
+		{ type: "input", name: "username", message: "Введи имя:" },
+		{
+			type: "list",
+			name: "bonuses1",
+			message: "Зачем в Питере солнечные батаерии?",
+			choices: [
+				{
+					name: "Вырабатавать энергию",
+					value: 0,
+					short: "Че оно там выработает",
+				},
+				{ name: "Для виду", value: 0, short: "Может и так" },
+				{ name: "Бесполезные", value: 5, short: "Ну да " },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses2",
+			message: "Кто основал Санкт-Петербург",
+			choices: [
+				{
+					name: "Петр I",
+					value: 5,
+					short: "С js у тебя не клеется, хоть это знаешь ",
+				},
+				{ name: "Шнуров", value: 0, short: "Это про Ленинград" },
+				{
+					name: "Сергей Дружко",
+					value: 0,
+					short: "Сильное заявление, но мимо ",
+				},
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses3",
+			message: "Чем славится питер?",
+			choices: [
+				{
+					name: "Петропавловская крепость",
+					value: 5,
+					short: "Все ответы правильные",
+				},
+				{ name: "Петергоф", value: 5, short: "Все ответы правильные" },
+				{ name: "Погодой", value: 5, short: "Все ответы правильные" },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses4",
+			message: "В какой стране  Деда Мороза называют Йоулупукки?",
+			choices: [
+				{ name: "Гавайи", value: 0, short: "не правильно" },
+				{ name: "Новая Зеландия", value: 0, short: "не правильно" },
+				{ name: "Финляндия", value: 5, short: "Сильное заявление, правильно " },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses5",
+			message: "Кто наряжается один раз в год?",
+			choices: [
+				{ name: "Ёлка", value: 5, short: "Ну а ты ее больше и не дастаешь" },
+				{ name: "Я", value: 0, short: "мимо" },
+				{ name: "Дед мороз", value: 0, short: "Мимо" },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses6",
+			message: "Где родилась елочка?",
+			choices: [
+				{ name: "В лесу", value: 5, short: "Ну а где еще?" },
+				{ name: "В роддоме", value: 0, short: "Ну почти" },
+				{ name: "В лифте", value: 0, short: "Неверно" },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses7",
+			message: "Чему можно научить дельфина?",
+			choices: [
+				{ name: "Ходить", value: 0, short: "Иди поспи" },
+				{ name: "Программировать", value: 0, short: "Не судьба" },
+				{ name: "Рисовать", value: 5, short: "Ну да" },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses8",
+			message: "Какое животное не боится никого?",
+			choices: [
+				{ name: "Лев", value: 0, short: "Ну почти" },
+				{ name: "Медоед", value: 5, short: "Ему вообще побарабану на всех" },
+				{ name: "Черепаха", value: 0, short: "Мимо" },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses9",
+			message: "Какое животное не умеет прыгать",
+			choices: [
+				{ name: "Черепаха", value: 0, short: "Могут, отвечаю, могут" },
+				{ name: "Слон", value: 5, short: "Так эта туша весит 3-4 тонны" },
+				{
+					name: "Турбированый ишак",
+					value: 0,
+					short: "Он не то что прыгать может, он летает",
+				},
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses10",
+			message: "Чего больше всего боятся программисты? ",
+			choices: [
+				{ name: "Когда они забывают сохранить", value: 5, short: "Правильно" },
+				{ name: "Толю", value: 0, short: "Мы все любим и уважаем Толю" },
+				{
+					name: "Что код не запустится",
+					value: 0,
+					short: "Переделаем, починим, подлатаем",
+				},
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses11",
+			message: "Какое золотое правило в программировании? ",
+			choices: [
+				{
+					name: "Если оно работает, не трогайте его.",
+					value: 5,
+					short: "Правильно",
+				},
+				{
+					name: "Пару раз сломать код",
+					value: 0,
+					short: "Это база, но не оно",
+				},
+				{ name: "Пропустить запятую", value: 0, short: "Дай бог найдется" },
+			],
+		},
+		{
+			type: "list",
+			name: "bonuses12",
+			message: "какие языки программирования самые дорогие?",
+			choices: [
+				{
+					name: "Ruby, Perl и Crystal",
+					value: 5,
+					short: "Вы эти названия вообще слышали? Очевидно же",
+				},
+				{ name: "C++, C#, C", value: 0, short: "Не звучит" },
+				{
+					name: "JavaScript, Java, Python",
+					value: 0,
+					short: "Простите, но вы названия вообще слышали их?",
+				},
+			],
+		},
+	]);
 }
 
-
 module.exports = { getQuestions };
-
-
-
 
 // class Question {
 //   #question
--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ const view = require('./view');
 async function run() {
   
     const promise = await model.getQuestions();
-    const allBonuses = promise.bonuses1+promise.bonuses2+promise.bonuses3
+    const allBonuses = promise.bonuses1+promise.bonuses2+promise.bonuses3+promise.bonuses4+promise.bonuses5+promise.bonuses6+promise.bonuses7+promise.bonuses8+promise.bonuses9+promise.bonuses10+promise.bonuses11+promise.bonuses12
     const phrase = `Молодец! Ты заработал ${allBonuses} баллов`
     view.showQuestions(phrase);
 }
